c19f831c371b9ad3f9476c2e4ad2b2903417d773
Nestor162 <113930607+Nestor162@users.noreply.github.com>
2024-08-22 16:39:36 +0200

    Fix focus on GamePanel problem
    with the addition of this line the focus is requested from the main
    thread solving the issues

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,10 @@
 package main;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -11,12 +12,18 @@ public class Main {
 		window.setTitle("JavaQuest2D");
 		window.setLocationRelativeTo(null);
 		window.setVisible(true);
-		
+
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
-		
+
 		window.pack();
-		
+
+		// Request focus to GamePanel, without this line focus owner was
+		// "javax.swing.JFrame"
+		// and keyHandler was not working at all.
+		// after this line, the focus owner is " main.GamePanel" and controls work
+		SwingUtilities.invokeLater(() -> gamePanel.requestFocusInWindow());
+
 		gamePanel.startGameThread();
 	}
 
