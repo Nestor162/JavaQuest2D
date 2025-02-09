41355c6fd823c568ad7526d667014d12b98595a8
Nestor162 <113930607+Nestor162@users.noreply.github.com>
2025-02-09 10:39:57 +0100

    Add new tiles and map

--- src/sprites/TileManager.java
+++ src/sprites/TileManager.java
@@ -47,9 +47,6 @@ public class TileManager {
 			tile[5] = new Tile(); // bush (tile 5)
 			tile[5].image = ImageIO.read(getClass().getResource("/world/overworld-single/tile1441.png"));
 
-			tile[6] = new Tile(); // sign (tile 6)
-			tile[6].image = ImageIO.read(getClass().getResource("/world/overworld-single/tile217.png"));
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -101,7 +98,12 @@ public class TileManager {
 			int screenX = worldX - gp.player.worldX + gp.player.screenX;
 			int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
-			g2.drawImage(tile[tileNum].image, screenX, screenY, tileSize, tileSize, null);
+			if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX
+					&& worldX - +gp.tileSize < gp.player.worldX + gp.player.screenX
+					&& worldY + gp.tileSize > gp.player.worldY - gp.player.screenY
+					&& worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
+				g2.drawImage(tile[tileNum].image, screenX, screenY, tileSize, tileSize, null);
+			}
 			worldCol++;
 
 			if (worldCol == gp.maxWorldCol) {
